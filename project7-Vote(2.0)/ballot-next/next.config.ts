fd4a2975bdb1a438a3b287f67b924d3c17c9c8fa
BlazE-09X <aruzansaparbek172@gmail.com>
2026-04-22 22:44:20 +0500

    project7

--- project7-Vote(2.0)/ballot-next/next.config.ts
+++ project7-Vote(2.0)/ballot-next/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/BlazE-09X/Blockchain_projects/project7-Vote(2.0)/frontend-web', 
+  basePath: '/Blockchain_projects/project7-Vote(2.0)/frontend-web', 
   images: {
     unoptimized: true,
   },
