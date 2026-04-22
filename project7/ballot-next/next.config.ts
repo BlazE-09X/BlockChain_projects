c4cc1e40c7b7627f2e4c60bed4e0bb97e118e6a6
BlazE-09X <aruzansaparbek172@gmail.com>
2026-04-23 00:14:08 +0500

    project7

--- project7/ballot-next/next.config.ts
+++ project7/ballot-next/next.config.ts
@@ -1,14 +1,8 @@
-import type { NextConfig } from "next";
-
-const repo = "/Blockchain_projects/project7/ballot-next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: repo,
-  assetPrefix: repo + "/",
-  images: {
-    unoptimized: true,
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // обязательно для GitHub Pages
+  basePath: '/Blockchain_projects/project7/ballot-next',
+  assetPrefix: '/Blockchain_projects/project7/ballot-next/',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
