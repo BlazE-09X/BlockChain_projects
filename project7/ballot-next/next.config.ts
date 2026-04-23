0abe62daef37f72249350dda4232934e5a328d13
BlazE-09X <aruzansaparbek172@gmail.com>
2026-04-23 22:45:42 +0500

    vercel deploy second trying, modified next.config.ts

--- project7/ballot-next/next.config.ts
+++ project7/ballot-next/next.config.ts
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // обязательно для GitHub Pages
-  basePath: '/Blockchain_projects/project7/ballot-next',
-  assetPrefix: '/Blockchain_projects/project7/ballot-next/',
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
